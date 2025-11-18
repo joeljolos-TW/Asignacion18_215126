c8d506a8553b810b89b4f2ee3da1d6587ec943b2
Joel Eduardo Cárdenas Beltrán <124020433+joeljolos-TW@users.noreply.github.com>
2025-11-18 12:59:25 -0700

    Add files via upload

---
+++ Asignacion18_215126/Asignacion18_215126/src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
+    static void main() {
+        MongoClient cliente = MongoClients.create("mongodb://localhost:27017/");
+        MongoDatabase db = cliente.getDatabase("restaurante");
+        MongoCollection<Document> col= db.getCollection("cafes");
+
+        Document cafe1= new Document();
+
+        cafe1.append("name","cafe de la plaza");
+        cafe1.append("stars",4.3);
+        cafe1.append("categories", Arrays.asList(new String[]{"Cafe","Postres","Desayuno"}));
+
+        col.insertOne(cafe1);
+
+        List<Document> cafes = new ArrayList<>();
+
+        cafes.add(
+                new Document("name","Espresso express")
+                        .append("stars",4.8)
+                        .append("categories", Arrays.asList(new String[]{"Cafe","Rapido","Takeaway"}))
+        );
+
+        cafes.add(
+                new Document("name","The Tea House")
+                        .append("stars",3.9)
+                        .append("categories", Arrays.asList(new String[]{"Te","Infusiones","Postres"}))
+        );
+
+        cafes.add(
+                new Document("name","Morning Brew")
+                        .append("stars",4.0)
+                        .append("categories", Arrays.asList(new String[]{"Cafe","Desayuno","Bakery"}))
+        );
+
+        col.insertMany(cafes);
+
+        for(Document d : col.find(Filters.gte("stars",4.5))){
+            System.out.println(d.toJson());
+        }
+
+        for(Document d : col.find(Filters.regex("name","")));
+    }
+}
